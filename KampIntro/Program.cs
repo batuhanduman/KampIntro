78399db60b3737f1d10f2368e5681c10273dc9e8
hello@batuhanduman.com <hello@batuhanduman.com>
2021-01-23 21:04:40 +0300

    20 Ocak Ders Sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            // do not repeat yourself - kendini tekrarlama
+            // değer tutucu,alias
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;  //float gibi küsürlü sayı
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+            
+        }
+    }
+}
